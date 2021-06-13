801647e46be8145a7db3ae3ff54ef7f35f09acbd
Дмитрий Зепсен <zepsendm@gmail.com>
2021-06-13 14:14:45 +0300

    Task 2 complete

--- modules/iam/policy_attachments/resource.attachments.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_organizations_policy_attachment" "restrict_regions_on_root" {
-  policy_id = var.policy_id
-  target_id = var.target_id
-}
--- modules/iam/sns_policy/resource.policy.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_sns_topic_policy" "account_billing_alarm_policy" {
-  arn    = var.arn
-  policy = var.policy
-}
