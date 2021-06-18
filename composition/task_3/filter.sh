46a0feffdc7df71987d43b0b216c004924d239f6
Дмитрий Зепсен <zepsendm@gmail.com>
2021-06-18 22:06:54 +0300

    test

--- composition/task_3/filter.sh
+++ composition/task_3/filter.sh
@@ -8,7 +8,7 @@ read bucket
 
 $keys=aws s3api list-objects --bucket $bucket --query 'Contents[?LastModified<=`{$date}`][].Key'
 
-for key in $keys
+for key in ${keys[*]}
 do
   aws s3api get-object --bucket $bucket --key $key new-$key
 done
