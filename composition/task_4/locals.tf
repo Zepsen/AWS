48e1008fbf67c869c5d740c0c2bb08c9d3552939
Дмитрий Зепсен <zepsendm@gmail.com>
2021-06-25 21:33:29 +0300

    create template

---
+++ composition/task_4/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  alias     = var.alias
+  region    = var.region
+  fst_email = var.fst_email
+  snd_email = var.snd_email
+}
---
+++ composition/template/modules.tf
@@ -0,0 +1,4 @@
+
+module "attach_read_only_s3_role" {
+  source = "../../modules/"
+}
